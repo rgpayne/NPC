d043f80977ad7c0cace9486a66ef9fcff66ba84a
Ross <sylent@gmail.com>
2015-02-13 16:43:19 -0600

    boolean[][] instead of int[][] and changed the backtracking

--- Graph.java
+++ Graph.java
@@ -12,18 +12,20 @@ import java.util.*;
 public class Graph {
     private int E; //number of edges
     private int V; //number of vertices
-    private int[][] G; //the graph itself
+    private boolean[][] G; //the graph itself
     private int mCliqueSize; //size of the max clique
     private Set<Integer> mClique; //the max clique itself
-    private long maxCliqueSpeed; //how long it took to find the max clique
+    private long mCliqueSpeed; //how long it took to find the max clique
 
     Graph() {}
 
     /*
-    Creates a graph from an adjacency matrix
+    Creates a graph from an adjacency matrix. Assumes the passed in adjacency matrix is valid but counts edges, vertices, etc.
      */
-    //untested
-    Graph(int[][] matrix) {
+
+
+    //UNTESTED
+    Graph(boolean[][] matrix) {
         this.G = matrix;
         V = matrix.length;
 
@@ -32,7 +34,7 @@ public class Graph {
         //counting number of edges. could be improved to only need to scan half the graph
         for (int i = 0; i < matrix.length; i ++) {
             for (int j = 0; j < matrix.length; j++) {
-                if (G[i][j] == 1) e++;
+                if (G[i][j]) e++;
             }
         }
         this.E = (e-V)/2; //no edges to onesself and each edge is counted twice
@@ -50,8 +52,6 @@ public class Graph {
                 //discard a graph with empty vertex set. i.e. when end of file is reached and the last line is 0 which causes readGraphs to abort
                 if (c.V == 0) continue;
                 col.add(c); //add to the collection of graphs
-                //c.printGraph();
-                //System.out.println();
             }
         } catch (FileNotFoundException e) {
             System.out.println("Scanner failed to open file");
@@ -78,12 +78,12 @@ public class Graph {
     }
 
     /*
-    Gives the info in the form as requested in the instructions (for the most part)
+    Gives the info in the form as requested in the instructions
      */
     @Override
     public String toString() {
         String mc = mClique.toString().replace('[','{').replace(']','}');
-        return "("+V+","+E+") "+ mc +" (size="+mCliqueSize+", "+maxCliqueSpeed+" ms)";
+        return "("+V+","+E+") "+ mc +" (size="+mCliqueSize+", "+mCliqueSpeed+" ms)";
     }
 
     /*
@@ -91,14 +91,14 @@ public class Graph {
      */
     public void readGraph(Scanner s) {
         int e = 0;
-        int r;
-        int g[][];
+        boolean r;
+        boolean g[][];
         int d = s.nextInt();
         if (d == 0) return;
-        g = new int[d][d];
+        g = new boolean[d][d];
         for (int i = 0; i < d; i++) {
             for (int j = 0; j < d; j++) {
-                if ((r = s.nextInt()) == 1) e++;
+                if ((r = (s.nextInt()) == 1)) e++;
                 g[i][j] = r;
             }
         }
@@ -107,65 +107,67 @@ public class Graph {
         this.V = d;
     }
 
-    /* Returns a set that represents the largest clique in G and times how long it took to find.
-    *  The idea is that for graph G of size |V| we find the power set of |V|. We then
-    *  go through every entry in the power set and check if each is a clique,
-    *  keeping track of the largest as we go.
-    * */
+    /* kicks off and times the recursion for the overloaded findLargestClique method */
     void findLargestClique() {
-        Set<Integer> R = new HashSet();
-        Set<Integer> P = new HashSet();
-        Set<Integer> X = new HashSet();
+        Set<Integer> empty = new HashSet();
+        Set<Integer> allNodes = new HashSet();
 
         mClique = new HashSet<Integer>();
         mClique.add(0);
         for (int i = 0; i < G.length; i++) {
-            P.add(i);
+            allNodes.add(i);
         }
         long start = System.currentTimeMillis();
-        bronKerbosch(R,P,X);
-        maxCliqueSpeed = System.currentTimeMillis()-start;
+        /* start off the recursion with current = empty set and candidates = all nodes in G */
+        findLargestClique(empty,allNodes);
+        mCliqueSpeed = System.currentTimeMillis()-start;
     }
 
     /*
-    An implementation of the vanilla Bron-Kerbosch algorithm
+    Finds maximum clique using recursive backtracking
+    current = the current clique we are building
+    candidates = the set of candidate vertices that may end up in r (initially the entire vertex set of G)
      */
-    void bronKerbosch(Set<Integer> r, Set<Integer> p, Set<Integer> x) {
-        if (p.isEmpty() && x.isEmpty()) {
-            if (mClique.size() < r.size()) {
-                mClique = r;
-                mCliqueSize = r.size();
+    void findLargestClique(Set<Integer> current, Set<Integer> candidates) {
+        /* if we've run out of candidate notes */
+        if (candidates.isEmpty()) {
+            /* keep track of largest clique found */
+            if (mClique.size() < current.size()) {
+                mClique = current;
+                mCliqueSize = current.size();
             }
         }
-        Iterator<Integer> iter = p.iterator();
+        Iterator<Integer> iter = candidates.iterator();
+        /* iterate through entire vertex set of G */
         while (iter.hasNext()){
             Integer vertex = iter.next();
-            Set<Integer> ruv = new HashSet(r);
-            ruv.add(vertex);
 
+            /* add vertex to current set */
+            Set<Integer> newCurrent = new HashSet(current);
+            newCurrent.add(vertex);
+
+            /* the neighbors of the vertex */
             Set<Integer> n = findNeighbors(vertex);
 
-            Set<Integer> pIn = new HashSet(p);
-            pIn.retainAll(n);
+            /* Intersect candidate nodes and neighbors of vertex */
+            Set<Integer> candidateNeighbors = new HashSet(candidates);
+            candidateNeighbors.retainAll(n);
 
-            Set<Integer> xIn = new HashSet(x);
-            xIn.retainAll(n);
-
-            bronKerbosch(ruv, pIn, xIn);
+            /* Call recursively */
+            findLargestClique(newCurrent, candidateNeighbors);
 
             iter.remove();
-            x.add(vertex);
         }
 
     }
 
     /*
-    Returns the set of vertexes connected to vertex
+    Returns the set of vertices connected to vertex
      */
     Set<Integer> findNeighbors(int vertex) {
         Set <Integer> r = new HashSet<Integer>();
         for (int i = 0; i < G.length; i++ ) {
-            if (G[vertex][i] == 1 && vertex != i) {
+            if (G[vertex][i] && vertex != i) {
                 r.add(i);
             }
         }
